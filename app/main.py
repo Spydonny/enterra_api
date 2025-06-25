70ea6e5ff960852341e5a3ff44b660f2dfd615f1
Spydonny <spydon64@gmail.com>
2025-06-26 00:31:33 +0500

    added cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from .routers import *
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 for r in [companies, users, posts, messages, reviews, auth]:
     app.include_router(r)
