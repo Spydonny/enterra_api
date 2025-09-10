c3ca30c9c742729ab8c395a4a2a81abd53064d21
Spydonny <spydon64@gmail.com>
2025-09-10 22:24:45 +0500

    hkghjkas

--- app/db.py
+++ app/db.py
@@ -4,4 +4,4 @@ from app.settings import settings
 from .schemas import UserInDB
 
 client = AsyncIOMotorClient(settings.mongo_uri, uuidRepresentation="standard")
-db = client["enterra_db-test"]
+db = client["enterra_db-tests"]
