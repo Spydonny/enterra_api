a1b58f82d617004234a070e828175321750fdb6a
Spydonny <spydon64@gmail.com>
2025-09-13 12:10:40 +0500

    jdsljfklsdjlk

--- app/db.py
+++ app/db.py
@@ -4,4 +4,4 @@ from app.settings import settings
 from .schemas import UserInDB
 
 client = AsyncIOMotorClient(settings.mongo_uri, uuidRepresentation="standard")
-db = client["db_prod0ucti0on0"]
+db = client["db_prod0ucti0on00"]
